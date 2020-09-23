d8e2ad6ed7d495f50053f997db866b4c89a1028b
Adriana Lima <adrianalimafm@gmail.com>
2020-09-23 09:32:34 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -14,9 +14,10 @@ function App() {
     async function getContent() {
       try {
         const response = await api.get(`repos/facebook/react`);
-        setRepository(response.data) // pego as informações utilizando o data e coloca no respository
+        const responseData = response.data
+        setRepository(responseData) 
 
-        console.log(response.data)
+        console.log(response.data.owner)
 
       } catch (error) {
         console.error(error)
@@ -27,29 +28,29 @@ function App() {
 
 
   // Consumindo API Utilizando o Fetch
-  const [planetData, setPlanetData] = useState('');
+  const [movieData, setMovieData] = useState('');
 
   useEffect(() => {
-
-    async function getContent() {
-      try {
-        const planetNumber = Math.floor(Math.random() * 61) + 1
-        const url = `https://swapi.dev/api/planets/${planetNumber}`
-
-        const response = await fetch(url)
-        const data = await response.json(); // precisa transformar as informações em Json e coloca no planetData
-
-        setPlanetData(data)
-
-      } catch (error) {
-        console.error(error)
-        const message = document.querySelector('main')
-        message.innerHTML = `<p class='error'>Desculpe, não foi possível carregar os dados.</p>`
-      }
-    }
-
-    getContent();
+    newMovie();
   }, []) 
+
+
+  async function newMovie(){
+    try {
+      const movieNumber = Math.floor(Math.random() * 6) + 1
+      const url = `https://swapi.dev/api/films/${movieNumber}`
+
+      const response = await fetch(url)
+      const data = await response.json(); 
+
+      setMovieData(data)
+
+    } catch (error) {
+      console.error(error)
+      const message = document.querySelector('main')
+      message.innerHTML = `<p class='error'>Desculpe, não foi possível carregar os dados.</p>`
+    }
+  }
 
     return (
       <div className="app">
@@ -63,19 +64,21 @@ function App() {
             <li>Descrição: {repository.description}</li>
             <li>Login: {repository.login}</li>
             <li>Open Issues: {repository.open_issues}</li>
-            <li>Size: {repository.owner.login}</li>
+            <li>Login: {repository.owner && repository.owner.login}</li>
           </section>
 
           
           <section>
-            <h1> Lista de Planetas do Star Wars</h1>
+            <h1> Lista de movies do Star Wars</h1>
             <p>Consumindo API do github utilizando o Fetch</p>
 
-            <li>Nome do Planeta: {planetData.name}</li>
-            <li>População: {planetData.population}</li>
-            <li>Clima: {planetData.climate}</li>
-            <li>Terreno: {planetData.terrain}</li>
+            <li>Nome do Filme: {movieData.title}</li>
+            <li>Episódio: {movieData.episode_id}</li>
+            <li>Diretor: {movieData.director}</li>
+            <li>Data de Lançamento: {movieData.release_date}</li>
           </section>
+
+          <button type="submit" onClick={newMovie}> Pesquisar </button>
 
         </main>
 
