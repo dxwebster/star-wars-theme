107d51201d34c16d36788279bbdc650e8211a46e
dxwebster <adrianalimafm@gmail.com>
2020-10-13 16:22:28 -0300

    Updates

--- src/pages/LightTheme/styles.js
+++ src/pages/LightTheme/styles.js
@@ -46,54 +46,52 @@ export const Content = styled.div`
 `;
 
 export const Card = styled.div`
+  display: flex;
+  flex-direction: row;
+  color: #fff;
+
+  .card {
+    width: 100%;
     display: flex;
-    flex-direction: row;
-    color: #fff;
+    justify-content: center;
 
-    .card {
-        width: 100%;
-        display: flex;
-        justify-content: center;
-        
-        .description {
-            width:70%;
-            padding: 50px;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
+    .description {
+      width: 70%;
+      padding: 50px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
 
-            h1 {
-                color: white;
-                font-size: 30px;
-                font-weight: 700;
-                padding-bottom: 20px;
-                text-transform: uppercase;
-            }
-    
-            li{
-                list-style: none;
-                padding: 5px 0
-            }
-        
-            button{
-                width: 200px;
-                margin-top: 50px;
-                cursor: pointer;
-                border: 1px solid #fff;
-                background: #000;
-                padding: 20px;
-                color: #fff;
-                text-transform: uppercase;
-                transition: 0.3s;
-    
-            &:hover{
-                border: 1px solid #f7b80a;
-                color: #f7b80a;
-                background: #252525
-            }
+      h1 {
+        color: white;
+        font-size: 30px;
+        font-weight: 700;
+        padding-bottom: 20px;
+        text-transform: uppercase;
+      }
 
+      li {
+        list-style: none;
+        padding: 5px 0;
+      }
+
+      button {
+        width: 200px;
+        margin-top: 50px;
+        cursor: pointer;
+        border: 1px solid #fff;
+        background: #000;
+        padding: 20px;
+        color: #fff;
+        text-transform: uppercase;
+        transition: 0.3s;
+
+        &:hover {
+          border: 1px solid #f7b80a;
+          color: #f7b80a;
+          background: #252525;
         }
-
-        
+      }
     }
+  }
 `;
